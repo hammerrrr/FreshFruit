d4125b7c9cc4c424de773897d36b1b3f5be9e979
Ivan Nur Ilham Syah <ivannurilham60@hotmail.com>
2020-11-12 16:09:35 +0700

    add fruit and seller model

---
+++ FreshFruit/model/Seller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FreshFruit.model
+{
+    class Seller
+    {
+        private string name;
+        private BucketController bucketController;
+
+        public Seller (string name, BucketController bucketController)
+        {
+            this.name = name;
+            this.bucketController = bucketController;
+        }
+
+        public List<Fruit> findAll ()
+        {
+            return this.bucketController.findAll();
+        }
+
+        public void addFruit (Fruit fruit)
+        {
+            this.bucketController.addFruit(fruit);
+        }
+    }
+}
